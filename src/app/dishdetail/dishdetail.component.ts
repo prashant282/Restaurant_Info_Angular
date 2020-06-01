ab66da3dd8c96ea2013b8b16929970384f7af6ce
prashant282 <prashantbhardwaj282@gmail.com>
2020-06-02 02:10:13 +0530

    SPA Part-2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,5 +1,8 @@
+import { DishService } from './../services/dish.service';
 import { Dish } from './../shared/dish';
 import { Component, OnInit, Input } from '@angular/core';
+import {Params, ActivatedRoute } from "@angular/router";
+import {Location } from "@angular/common";
 
 
 @Component({
@@ -9,12 +12,20 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class DishdetailComponent implements OnInit {
 
-  @Input()
+  // @Input()
   dish:Dish;
 
-  constructor() { }
+  constructor(private dishService: DishService,
+    private location: Location,
+    private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+    let id= this.route.snapshot.params['id'];
+    this.dish= this.dishService.getDish(id);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
