c189b48338f8e92f342561fb765f867c46d0ccba
prashant282 <prashantbhardwaj282@gmail.com>
2020-06-12 01:51:10 +0530

    Promise Part-2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import {FormsModule } from '@angular/forms';
 import {ReactiveFormsModule } from '@angular/forms';
 import {MatSelectModule } from '@angular/material/select';
 import {MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
@@ -61,7 +62,8 @@ import { LoginComponent } from './login/login.component';
     MatCheckboxModule,
     ReactiveFormsModule,
     MatSelectModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    MatProgressSpinnerModule
   ],
   providers: [
     DishService,
